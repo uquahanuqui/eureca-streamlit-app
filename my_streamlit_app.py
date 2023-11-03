93715c8e472f0d155919ad230c31e2c9a8070f90
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-03 13:25:13 +0900

    Add files via upload

---
+++ my_streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+#타이틀 & 이모티콘
+st.title("안뇽! :fish_cake: :yellow_heart: :snowman:")
+
+#일반 글씨
+st.header("유쾌한육회")
+st.write("우리는 6조")
+st.text("렌즈추천사이트")
+
+#버튼 클릭
+clicked=st.button("click")
+
+#이미지 삽입
