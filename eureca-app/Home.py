8f647a11e5046f9090af0bc368447bad5c508226
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-10 01:05:48 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -22,3 +22,22 @@ st.link_button("HapaKristin", "https://hapakristin.co.kr/")
 
 #눈내림
 st.snow()
+
+#페이지
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
