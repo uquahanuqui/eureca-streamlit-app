09ba1644b811146f2664669ce9e5d7fe167520ec
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-09 14:53:43 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -3,9 +3,14 @@ import streamlit as st
 #타이틀 & 이모티콘
 st.title("안뇽! :snowman:")
 
+#구분선
+st.divider()
+
 #일반 글씨
 st.header("AI 퍼스널 렌즈 추천 플랫폼")
 st.write("6조 : 유쾌한육회")
+
+st.divider()
 
 #버튼 클릭
 clicked=st.button("Go Shopping")
