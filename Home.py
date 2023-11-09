c629ce7b07b7005a40864134a2e091b2c27ae9ad
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-09 14:11:29 +0900

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+#타이틀 & 이모티콘
+st.title("안뇽! :snowman:")
+
+#일반 글씨
+st.header("유쾌한육회")
+st.write("우리는 6조")
+st.text("렌즈추천사이트")
+
+#버튼 클릭
+clicked=st.button("click")
+
+#이미지 삽입
+
+#사이드바
+with st.sidebar:
+    shop=st.text_input(
+        "Shop"
+    )
+    ai=st.text_input(
+        "AI Personal"
+    )
+    community=st.text_input(
+        "Community"
+    )
+    notice=st.text_input(
+        "Notice"
+    )
