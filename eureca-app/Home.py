a9648f6c4944dcc0da300009be44d57cf0e2a0db
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-10 17:41:32 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -16,15 +16,3 @@ st.snow()
 #검색창
 st.markdown("---")
 search_query = st.text_input("Search :mag:")
-
-#이미지 삽입
-from PIL import Image
-
-# Use a raw string for the file path
-image_path = r"/jwy.jpg"
-
-# Load the image into the Streamlit app
-image = Image.open("https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg")
-
-# Display the image in the Streamlit app
-st.image(image, caption='A caption for your image', use_column_width='always')
