16185b530900317274140d09a395be13921a701e
susoiwer <148053290+susoiwer@users.noreply.github.com>
2023-11-10 16:32:39 +0900

    Update Shop.py

--- eureca-app/pages/Shop.py
+++ eureca-app/pages/Shop.py
@@ -22,13 +22,36 @@ with col4:
     st.link_button("HapaKristin", "https://hapakristin.co.kr/")
 
 
-import streamlit as st
 import pandas as pd
-import numpy as np
+import streamlit as st
 
-df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+# Cache the dataframe so it's only loaded once
+@st.cache_data
+def load_data():
+    return pd.DataFrame(
+        {
+            "first column": [퓨어비전2 멀티포컬
+바이오피니티 토릭
+에어옵틱스 토릭
+나이트앤데이
+울트라 토릭
+바이오피니티
+퓨어비전2
+옵티마 FW
+울트라
+],
+            "second column": [10, 20, 30, 40],
+        }
+    )
 
-st.dataframe(df)
+# Boolean to resize the dataframe, stored as a session state variable
+st.checkbox("Use container width", value=False, key="use_container_width")
+
+df = load_data()
+
+# Display the dataframe and allow the user to stretch the dataframe
+# across the full width of the container, based on the checkbox value
+st.dataframe(df, use_container_width=st.session_state.use_container_width)
 
 
     
