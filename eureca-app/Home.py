cb02a4c3af1f30c078c2d0a4c86342481fbdc0b0
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-10 16:11:35 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -13,9 +13,9 @@ st.write("Team 6 : uquahanuqui :christmas_tree:")
 #이미지 삽입
 from PIL import Image
 
-image_path = r"C:\\Users\\User\\Downloads\\jwy.jpg"
+image_path = r"C:\Users\User\Downloads\jwy.jpg"
 image = Image.open(image_path)
-st.image(image)
+st.image(image, use_column_width='always')
 
 #눈내림
 st.snow()
