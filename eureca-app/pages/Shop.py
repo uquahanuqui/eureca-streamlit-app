b3ee52d5bf8cfa1a461133fbe22020a3e908459c
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-10 12:46:05 +0900

    Update Shop.py

--- eureca-app/pages/Shop.py
+++ eureca-app/pages/Shop.py
@@ -5,3 +5,17 @@ st.title("Shop")
 
 #구분선
 st.divider()
+
+#버튼 클릭
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.link_button("Lens-me", "https://www.lens-me.com/shop/")
+
+with col2:
+    st.link_button("O-lens", "https://o-lens.com/")
+
+with col3:
+    st.link_button("Idol-lens", "https://www.i-dol.kr/")
+
+with col4:
+    st.link_button("HapaKristin", "https://hapakristin.co.kr/")
