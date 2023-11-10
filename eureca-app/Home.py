4db8b8d41310e5b7360c9273b06c04abc5486bd9
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-10 21:29:21 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -1,13 +1,12 @@
 import streamlit as st
 
 #타이틀 & 이모티콘
-st.markdown("<h1 style='text-align: center;'>❄️Lens in Gray❄️</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>☃️Lens in Gray☃️</h1>", unsafe_allow_html=True)
 
 #구분선
 st.divider()
 
 #일반 글씨
-st.markdown("<h1 style='text-align: center;'>☃️</h1>", unsafe_allow_html=True)
 st.write("Team 6 : uquahanuqui :christmas_tree:")
 
 #눈내림
