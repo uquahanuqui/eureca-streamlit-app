96d1a767dee746a89a13364ec9b09c4f07984ea7
susoiwer <148053290+susoiwer@users.noreply.github.com>
2023-11-10 16:48:21 +0900

    Update Shop.py

--- eureca-app/pages/Shop.py
+++ eureca-app/pages/Shop.py
@@ -24,7 +24,6 @@ with col4:
 
 
 st.dataframe(data=None, width=None, height=None, *, use_container_width=False, hide_index=None, column_order=None, column_config=None)
-st.dataframe("https://www.lens-me.com/shop/goods_detail.php?ps_uid=66178","https://image.bmit.co.kr/lens-me.com/upload/goods/2023090412044143512/2023090412044143512_0_400.jpg","https://image.bmit.co.kr/lens-me.com/upload/goods/2023090412044143512/2023090412044143512_1_400.jpg","유스 쵸코","13.0mm","25,000","1+1")
 
 
     
