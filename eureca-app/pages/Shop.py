a3b16c68beb69708a316b4e998793e62bdfedc9f
susoiwer <148053290+susoiwer@users.noreply.github.com>
2023-11-10 16:47:37 +0900

    Update Shop.py

--- eureca-app/pages/Shop.py
+++ eureca-app/pages/Shop.py
@@ -22,32 +22,9 @@ with col4:
     st.link_button("HapaKristin", "https://hapakristin.co.kr/")
 
 
-import pandas as pd
-import streamlit as st
 
-import pandas as pd
-import streamlit as st
-
-data_df = pd.DataFrame(
-    {
-        "apps": [
-            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
-            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/ef9a7627-13f2-47e5-8f65-3f69bb38a5c2/Home_Page.png",
-            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/31b99099-8eae-4ff8-aa89-042895ed3843/Home_Page.png",
-            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/6a399b09-241e-4ae7-a31f-7640dc1d181e/Home_Page.png",
-        ],
-    }
-)
-
-st.data_editor(
-    data_df,
-    column_config={
-        "apps": st.column_config.ImageColumn(
-            "Preview Image", help="Streamlit app preview screenshots"
-        )
-    },
-    hide_index=True,
-)
+st.dataframe(data=None, width=None, height=None, *, use_container_width=False, hide_index=None, column_order=None, column_config=None)
+st.dataframe("https://www.lens-me.com/shop/goods_detail.php?ps_uid=66178","https://image.bmit.co.kr/lens-me.com/upload/goods/2023090412044143512/2023090412044143512_0_400.jpg","https://image.bmit.co.kr/lens-me.com/upload/goods/2023090412044143512/2023090412044143512_1_400.jpg","유스 쵸코","13.0mm","25,000","1+1")
 
 
     
