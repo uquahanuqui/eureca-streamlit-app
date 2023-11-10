5b3cf9c7128a566f1535de83c93880854355b0a7
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-10 17:33:09 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -9,7 +9,6 @@ st.divider()
 #일반 글씨
 st.header("Lens in Gray")
 st.write("Team 6 : uquahanuqui :christmas_tree:")
-    
 
 #눈내림
 st.snow()
@@ -17,3 +16,15 @@ st.snow()
 #검색창
 st.markdown("---")
 search_query = st.text_input("Search :mag:")
+
+#이미지 삽입
+from PIL import Image
+
+# Use a raw string for the file path
+image_path = r"/jwy.jpg"
+
+# Load the image into the Streamlit app
+image = Image.open("https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg")
+
+# Display the image in the Streamlit app
+st.image(image, caption='A caption for your image', use_column_width='always')
