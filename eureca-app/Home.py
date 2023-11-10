5cb8f9cca13cb1725db561c24f5cc1532e85f679
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-10 16:18:35 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -13,7 +13,7 @@ st.write("Team 6 : uquahanuqui :christmas_tree:")
 #이미지 삽입
 from PIL import Image
 
-image_path = r"C:\Users\User\Downloads\jwy.jpg"
+image_path = "C:\Users\User\Downloads\jwy.jpg"
 image = Image.open(image_path)
 st.image(image, use_column_width='always')
 
