23f2d172cb632ad02206d89a0df1f4cb7225518c
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-10 17:25:19 +0900

    Update Home.py
    ddd

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -13,11 +13,6 @@ st.write("Team 6 : uquahanuqui :christmas_tree:")
 #이미지 삽입
 from PIL import Image
 
-# Use a raw string for the file path
-image_path = r"/jwy.jpg"
-
-# Load the image into the Streamlit app
-image = Image.open("https://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg")
 
 # Display the image in the Streamlit app
 st.image(image, caption='A caption for your image', use_column_width='always')
