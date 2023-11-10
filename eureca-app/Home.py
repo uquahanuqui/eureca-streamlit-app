ab3fc241691d03ba5d84259baed8326b2691d7fb
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-10 16:15:20 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -17,6 +17,13 @@ image_path = r"C:\Users\User\Downloads\jwy.jpg"
 image = Image.open(image_path)
 st.image(image, use_column_width='always')
 
+import os
+
+if os.path.exists(image_path):
+    image = Image.open(image_path)
+else:
+    print(f"The file {image_path} does not exist.")
+
 #눈내림
 st.snow()
 
