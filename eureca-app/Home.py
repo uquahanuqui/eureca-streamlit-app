d376e00d6d7491b083fffa5f45d22c8ac89a09a0
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-10 12:45:53 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -9,20 +9,6 @@ st.divider()
 #일반 글씨
 st.header("AI personal lens recommendation platform")
 st.write("6 : uquahanuqui :christmas_tree:")
-
-#버튼 클릭
-col1, col2, col3, col4 = st.columns(4)
-with col1:
-    st.link_button("Lens-me", "https://www.lens-me.com/shop/")
-
-with col2:
-    st.link_button("O-lens", "https://o-lens.com/")
-
-with col3:
-    st.link_button("Idol-lens", "https://www.i-dol.kr/")
-
-with col4:
-    st.link_button("HapaKristin", "https://hapakristin.co.kr/")
     
 #이미지 삽입
 
