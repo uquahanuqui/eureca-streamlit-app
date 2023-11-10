ec8626f385907ce5873ffe646e9923247c20ad35
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-10 12:19:56 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -13,11 +13,19 @@ st.write("6조 : 유쾌한육회")
 st.divider()
 
 #버튼 클릭
-st.link_button("Lens-me", "https://www.lens-me.com/shop/")
-st.link_button("O-lens", "https://o-lens.com/")
-st.link_button("Idol-lens", "https://www.i-dol.kr/")
-st.link_button("HapaKristin", "https://hapakristin.co.kr/")
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+    st.link_button("Lens-me", "https://www.lens-me.com/shop/")
 
+with col2:
+    st.link_button("O-lens", "https://o-lens.com/")
+
+with col3:
+    st.link_button("Idol-lens", "https://www.i-dol.kr/")
+
+with col4:
+    st.link_button("HapaKristin", "https://hapakristin.co.kr/")
+    
 #이미지 삽입
 
 #눈내림
