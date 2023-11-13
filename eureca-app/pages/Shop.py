ef50bb6d229fe5d26a318a4267627e081345777f
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-13 18:06:11 +0900

    Update Shop.py

--- eureca-app/pages/Shop.py
+++ eureca-app/pages/Shop.py
@@ -48,13 +48,13 @@ st.divider()
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.write("<div style='display: flex; margin-right: 200px;'><a href='https://www.lens-me.com/shop/' style='font-size: 14px; text-align: center;'>INSTAGRAM</a></div>", unsafe_allow_html=True)
+    st.write("<div style='display: flex; margin-left: 200px;'><a href='https://www.lens-me.com/shop/' style='font-size: 14px; text-align: center;'>INSTAGRAM</a></div>", unsafe_allow_html=True)
 
 with col2:
     st.write("<div style='display: flex; justify-content: center;'><a href='https://o-lens.com/' style='font-size: 14px; text-align: center;'>BLOG</a></div>", unsafe_allow_html=True)
 
 with col3:
-    st.write("<div style='display: flex; margin-left: 200px;'><a href='https://www.i-dol.kr/' style='font-size: 14px; text-align: center;'>KAKAO CHANNEL</a></div>", unsafe_allow_html=True)
+    st.write("<div style='display: flex; margin-right: 200px;'><a href='https://www.i-dol.kr/' style='font-size: 14px; text-align: center;'>KAKAO CHANNEL</a></div>", unsafe_allow_html=True)
 
     
 st.write(" ")
