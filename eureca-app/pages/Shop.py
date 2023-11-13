5f86a6d986b634a5144917ea8d0b98fcb89bc4c5
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-14 00:08:35 +0900

    Update Shop.py

--- eureca-app/pages/Shop.py
+++ eureca-app/pages/Shop.py
@@ -47,9 +47,9 @@ if __name__ == "__main__":
 st.divider()
 col1, col2, col3 = st.columns(3)
 
- with col1:
+with col1:
     st.write("<div style='display: flex; margin-left: 220px;'><a href='https://www.lens-me.com/shop/' style='font-size: 14px; text-align: center; color: black;'>INSTAGRAM</a></div>", unsafe_allow_html=True)
-     
+
 with col2:
     st.write("<div style='display: flex; justify-content: center; margin-left: -25px;'><a href='https://o-lens.com/' style='font-size: 14px; text-align: center; color: black;'>BLOG</a></div>", unsafe_allow_html=True)
 
@@ -68,4 +68,3 @@ st.write("<p style='text-align: center; margin: 5px; line-height: 0.8;'><span st
 st.caption("<p style='text-align: center; margin: 5px; line-height: 0.8;'>#77 Kookmin University Management Hall, Jeongneung-ro, Seongbuk-gu, Seoul</p>", unsafe_allow_html=True)
 st.caption("<p style='text-align: center; margin: 5px; line-height: 0.8;'>서울특별시 성북구 정릉로 77 국민대학교 경영관 (02707)</p>", unsafe_allow_html=True)
 st.divider()
-
