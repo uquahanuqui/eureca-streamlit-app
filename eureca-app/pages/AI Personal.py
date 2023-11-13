0ebd7d52104e5b2bd61da636b7ec8b7a6c8ef323
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-13 18:30:37 +0900

    Update AI Personal.py

--- eureca-app/pages/AI Personal.py
+++ eureca-app/pages/AI Personal.py
@@ -8,6 +8,18 @@ st.divider()
 
 #프로필
 st.divider()
+st.divider()
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write("<div style='display: flex; margin-left: 220px;'><a href='https://www.lens-me.com/shop/' style='font-size: 14px; text-align: center; color: black;'>INSTAGRAM</a></div>", unsafe_allow_html=True)
+
+with col2:
+    st.write("<div style='display: flex; justify-content: center; margin-left: -25px;'><a href='https://o-lens.com/' style='font-size: 14px; text-align: center; color: black;'>BLOG</a></div>", unsafe_allow_html=True)
+
+with col3:
+    st.write("<div style='display: flex; margin-left: -95px;'><a href='https://www.i-dol.kr/' style='font-size: 14px; text-align: center; color: black;'>KAKAO CHANNEL</a></div>", unsafe_allow_html=True)
+  
 st.write("<p style='text-align: center; margin: 5px; line-height: 0.8;'><span style='font-size: 15px;'>Lens in Gray</span> <span style='font-size: 15px;'>Co.</span> <span style='font-size: 12px;'>Owner</span> <span style='font-size: 15px;'>Uquahanuqui</span> </p>", unsafe_allow_html=True)
 st.write("<p style='text-align: center; margin: 5px; line-height: 0.8;'><span style='font-size: 12px;'>Business Number</span> <span style='font-size: 15px;'>20230922</span> </p>", unsafe_allow_html=True)
 st.write(" ")
