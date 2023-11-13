cda89975d3eb2ae5f3f838dd061b557edeca1b5e
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-13 12:10:45 +0900

    Update Guide.py

--- eureca-app/pages/Guide.py
+++ eureca-app/pages/Guide.py
@@ -6,6 +6,9 @@ st.markdown("<h1 style='text-align: center;'>Guide</h1>", unsafe_allow_html=True
 #구분선
 st.divider()
 
+
+st.header("<p style='text-align: center; margin: 5px; line-height: 0.8;'><span style='font-size: 30px;'>회원가입 안내</span> </p>", unsafe_allow_html=True)
+
 #내용
 st.header("회원가입 안내")
 st.write(" ")
