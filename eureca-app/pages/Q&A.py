17d4cdd25d93ac5a7f0cfde92eeedd2c9c8d6464
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-13 12:30:32 +0900

    Update Q&A.py

--- eureca-app/pages/Q&A.py
+++ eureca-app/pages/Q&A.py
@@ -7,14 +7,14 @@ st.markdown("<h1 style='text-align: center;'>Q&A</h1>", unsafe_allow_html=True)
 st.divider()
 
 #글씨
-st.markdown("<p style='font-size: 30px; font-weight: bold;'>FAQ</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 30px; font-weight: bold; margin: 0px 0;'>FAQ</p>", unsafe_allow_html=True)
 st.markdown("---")
 st.write("Q. 실시간 문의가 가능한가요?")
 st.write("Q. AI Personal 추천이 정확한가요?")
 st.write("Q. 오프라인 쇼룸이 있나요?")
 
 st.markdown("---")
-st.markdown("<p style='font-size: 30px; font-weight: bold;'>Q&A</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 30px; font-weight: bold; margin: 0px 0;'>Q&A</p>", unsafe_allow_html=True)
 st.markdown("---")
 st.write("‣ 주문 문의 :lock:")
 st.caption("→ RE: 주문 문의 :lock:")
@@ -22,12 +22,6 @@ st.write("‣ 기타 문의 :lock:")
 st.caption("→ RE: 기타 문의 :lock:")
 st.write("‣ 상품 문의 :lock:")
 st.caption("→ RE: 상품 문의 :lock:")
-
-
-st.markdown("---")
-st.markdown("<p style='font-size: 30px; font-weight: bold; margin: 0px 0;'>Q&A</p>", unsafe_allow_html=True)
-st.markdown("---")
-
 
 #프로필
 st.divider()
