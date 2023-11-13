11a6fdf7180005925a6cdf7d66d08decfa6a6035
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-13 14:15:14 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -10,9 +10,23 @@ st.divider()
 st.write("Team 6 : uquahanuqui :christmas_tree:")
 
 #이미지 첨부
-img_url = "https://image.ajunews.com/content/image/2023/03/07/20230307002159792573.jpg"
-st.image(img_url)
+img_url = "https://fimg5.pann.com/new/download.jsp?FileID=65745091"
 
+# 이미지를 중앙에 위치시키기 위한 CSS 스타일
+centered_image_style = """
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 400px; /* 이미지 높이 조절 */
+"""
+
+# 이미지를 스타일링하여 중앙에 표시
+st.markdown(
+    f'<div style="{centered_image_style}">'
+    f'<img src="{img_url}" alt="Image" style="max-width: 100%; max-height: 100%;">'
+    '</div>',
+    unsafe_allow_html=True
+)
 
 #눈내림
 st.snow()
