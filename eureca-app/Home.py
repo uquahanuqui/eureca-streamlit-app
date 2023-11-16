c7c4e62b34ca0e8b713ffe09be7bfefd1004767a
chlwngml <150420850+chlwngml@users.noreply.github.com>
2023-11-16 14:23:09 +0900

    Update Home.py

--- eureca-app/Home.py
+++ eureca-app/Home.py
@@ -39,14 +39,14 @@ search_query = st.text_input("Search :mag:")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.write("<div style='display: flex; margin-left: 220px;'><a href='https://www.lens-me.com/shop/' style='font-size: 14px; text-align: center; color: black;'>INSTAGRAM</a></div>", unsafe_allow_html=True)
+    st.markdown("<div style='position: fixed; left: 220px; top: 50px;’><a href='https://www.lens-me.com/shop/' style='font-size: 14px; text-align: center; color: black;'>INSTAGRAM</a></div>", unsafe_allow_html=True)
 
 with col2:
-    st.write("<div style='display: flex; justify-content: center; margin-left: -25px;'><a href='https://o-lens.com/' style='font-size: 14px; text-align: center; color: black;'>BLOG</a></div>", unsafe_allow_html=True)
+    st.markdown("<div style='position: fixed; left: 325px; top: 50px;’><a href='https://o-lens.com/' style='font-size: 14px; text-align: center; color: black;'>BLOG</a></div>", unsafe_allow_html=True)
 
 with col3:
-    st.write("<div style='display: flex; margin-left: -95px;'><a href='https://www.i-dol.kr/' style='font-size: 14px; text-align: center; color: black;'>KAKAO CHANNEL</a></div>", unsafe_allow_html=True)
-    
+    st.markdown("<div style='position: fixed; left: 435px; top: 50px;’><a href='https://www.i-dol.kr/' style='font-size: 14px; text-align: center; color: black;'>KAKAO CHANNEL</a></div>", unsafe_allow_html=True)
+ 
     
 st.write(" ")
 st.write("<p style='text-align: center; margin: 5px; line-height: 0.8;'><span style='font-size: 15px;'>Lens in Gray</span> <span style='font-size: 15px;'>Co.</span> <span style='font-size: 12px;'>Owner</span> <span style='font-size: 15px;'>Uquahanuqui</span> </p>", unsafe_allow_html=True)
