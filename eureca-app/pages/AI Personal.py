bbf5a4a40d4574a9be17fc9de321962c97ec0dc0
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-17 13:59:17 +0900

    Update AI Personal.py

--- eureca-app/pages/AI Personal.py
+++ eureca-app/pages/AI Personal.py
@@ -17,39 +17,34 @@ st.write("""
     body {
         background-color: white;
     }
-    .stApp {
+    .btn-container {
         display: flex;
-        justify-content: space-between;  # 버튼들을 왼쪽부터 오른쪽으로 나란히 정렬합니다.
-        align-items: center;  # 버튼들을 수직 가운데 정렬합니다.
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        background-color: transparent;
-        padding: 10px 20px;  # 좌우 여백을 조절합니다.
+        justify-content: center;  # 가운데 정렬
     }
     .btn-link {
         font-size: 14px;
         text-align: center;
         color: black;
-        margin: 0;  # 텍스트 간의 간격을 없앱니다.
-        padding: 0;  # 버튼 주변의 여백을 없앱니다.
-        text-decoration: none;  # 밑줄을 제거합니다.
+        background-color: transparent;  # 배경색을 투명하게 설정
+        border: none;  # 테두리 제거
+        padding: 5px 10px;  # 버튼 주변의 여백 설정
+        margin: 0 5px;  # 각 버튼 사이의 간격 조절
     }
 </style>
 """, unsafe_allow_html=True)
 
 # 세 개의 버튼 생성
 st.markdown("""
-<div class="stApp">
+<div class="btn-container">
     <a href='https://www.instagram.com/lensingray.official/' 
-       class='btn-link' style='color: black;'>INSTAGRAM</a>
+       class='btn-link'>INSTAGRAM</a>
     <a href='https://o-lens.com/' 
-       class='btn-link' style='color: black;'>BLOG</a>
+       class='btn-link'>BLOG</a>
     <a href='http://pf.kakao.com/_HBxnbG' 
-       class='btn-link' style='color: black;'>KAKAO CHANNEL</a>
+       class='btn-link'>KAKAO CHANNEL</a>
 </div>
 """, unsafe_allow_html=True)
+
 
 st.write(" ")
 st.write("<p style='text-align: center; margin: 5px; line-height: 0.8;'><span style='font-size: 15px;'>Lens in Gray</span> <span style='font-size: 15px;'>Co.</span> <span style='font-size: 12px;'>Owner</span> <span style='font-size: 15px;'>Uquahanuqui</span> </p>", unsafe_allow_html=True)
