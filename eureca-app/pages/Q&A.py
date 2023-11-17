efd2a9f0517650d7c05f1b7c0811e35ef333c437
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-11-17 15:54:34 +0900

    Update Q&A.py

--- eureca-app/pages/Q&A.py
+++ eureca-app/pages/Q&A.py
@@ -15,6 +15,8 @@ st.divider()
 #글씨
 st.markdown("<p style='font-size: 27px; font-weight: bold; margin: 0px 0;'>FAQ</p>", unsafe_allow_html=True)
 st.markdown("---")
+with st.expander("접힌 칸을 열어보세요"):
+    st.write("이 칸은 기본적으로 접혀있습니다.")
 st.write("Q. 실시간 문의가 가능한가요?")
 st.write("Q. AI Personal 추천이 정확한가요?")
 st.write("Q. 오프라인 쇼룸이 있나요?")
