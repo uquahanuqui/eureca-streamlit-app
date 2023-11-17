850b578e6802ababf3d8da0dc218f1a09b663e69
susoiwer <148053290+susoiwer@users.noreply.github.com>
2023-11-17 12:27:51 +0900

    Update Shop.py

--- eureca-app/pages/Shop.py
+++ eureca-app/pages/Shop.py
@@ -43,6 +43,23 @@ if __name__ == "__main__":
     main()
 
 
+import streamlit as st
+from PIL import Image
+
+# 타이틀 설정
+st.title('PNG 파일 뷰어')
+
+# 파일 업로드 부분
+uploaded_file = st.file_uploader("파일을 업로드하세요", type=['png'])
+
+if uploaded_file is not None:
+    # 파일을 읽고 이미지로 변환
+    image = Image.open(uploaded_file)
+    # 이미지 표시
+    st.image(image, caption='업로드된 이미지', use_column_width=True)
+
+
+
 #프로필
 st.divider()
 col1, col2, col3 = st.columns(3)
