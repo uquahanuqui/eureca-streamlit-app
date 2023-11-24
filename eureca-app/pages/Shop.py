684e6959ed8b3cf2e3d7283de1085dda611be635
susoiwer <148053290+susoiwer@users.noreply.github.com>
2023-11-24 16:02:30 +0900

    Update Shop.py

--- eureca-app/pages/Shop.py
+++ eureca-app/pages/Shop.py
@@ -202,8 +202,25 @@ def create_streamlit_app():
     elif category == "O-lens":
         st.header("O-lens Product")
         
-        
-        https://o-lens.com/product/20662
+        #이미지 첨부
+st.write(" ")
+img_url = "https://o-lens.com/product/20662"
+
+# 이미지를 중앙에 위치시키기 위한 CSS 스타일
+centered_image_style = """
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 400px; /* 이미지 높이 조절 */
+"""
+
+# 이미지를 스타일링하여 중앙에 표시
+st.markdown(
+    f'<div style="{centered_image_style}">'
+    f'<img src="{img_url}" alt="Image" style="max-width: 100%; max-height: 100%;">'
+    '</div>',
+    unsafe_allow_html=True
+)
 
         
         
