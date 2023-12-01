1d1d7a65118df53f40d332699a03a79d2bdb4cc0
yebin <148053191+uquahanuqui@users.noreply.github.com>
2023-12-01 15:34:41 +0900

    Update AI Personal.py

--- eureca-app/pages/AI Personal.py
+++ eureca-app/pages/AI Personal.py
@@ -17,6 +17,9 @@ st.divider()
 st.link_button("카카오 채널", "http://pf.kakao.com/_xblxexjG")
 st.link_button("채팅 바로가기", "http://pf.kakao.com/_xblxexjG/chat")
 
+# iframe을 통해 티처블 머신 웹 페이지를 포함시킬 공간을 만듭니다.
+st.markdown('<iframe src="https://teachablemachine.withgoogle.com/models/Y44cpwtyV/" width="800" height="600"></iframe>', unsafe_allow_html=True)
+
 # 스트림릿 앱 제목
 st.header("Teachable Machine Image Classification")
 
